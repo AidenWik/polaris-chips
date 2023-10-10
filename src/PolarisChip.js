e77ce990a0881e4f5ff31c9352e067a86dac6630
AidenWik <142683824+AidenWik@users.noreply.github.com>
2023-10-10 12:37:42 -0400

    a

--- src/PolarisChip.js
+++ src/PolarisChip.js
@@ -29,6 +29,6 @@ export class PolarisChip extends LitElement {
   }
 
   render() {
-    return html`<span>${this.name}<slot></slot></span>`;
+    return html`<span><slot>${this.name}</slot></span>`;
   }
 }
